74dbd200b488f2b14a7dd298eb3e36c38da6ddb9
Rakshita Chauhan <chauhanrakshita64@gmail.com>
2026-04-29 03:33:41 +0530

    Refactor length measurement to weight measurement

--- src/main/java/com/quantity/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/QuantityMeasurementApp.java
@@ -1,35 +1,45 @@
 package com.quantity;
 
-enum LengthUnit {
-    FEET(12.0), INCHES(1.0), YARD(36.0), CM(0.393701);
+enum WeightUnit {
+    KG(1.0), GRAM(0.001), POUND(0.453592);
     private final double factor;
-    LengthUnit(double f) { this.factor = f; }
-
-    public double toBase(double value) { return value * factor; }
-    public double fromBase(double baseValue) { return baseValue / factor; }
+    WeightUnit(double f) { this.factor = f; }
+    public double toBase(double v) { return v * factor; }
+    public double fromBase(double bv) { return bv / factor; }
 }
 
-class QuantityLength {
+class QuantityWeight {
     private final double value;
-    private final LengthUnit unit;
+    private final WeightUnit unit;
 
-    public QuantityLength(double v, LengthUnit u) {
+    public QuantityWeight(double v, WeightUnit u) {
         this.value = v;
         this.unit = u;
     }
 
-    public double getBaseValue() { return unit.toBase(value); }
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof QuantityWeight)) return false;
+        QuantityWeight that = (QuantityWeight) obj;
+        return Math.abs(this.unit.toBase(this.value) - that.unit.toBase(that.value)) < 0.001;
+    }
 
-    public static QuantityLength add(QuantityLength q1, QuantityLength q2, LengthUnit target) {
-        double sum = q1.getBaseValue() + q2.getBaseValue();
-        return new QuantityLength(target.fromBase(sum), target);
+    public static QuantityWeight add(QuantityWeight w1, QuantityWeight w2, WeightUnit target) {
+        double sum = w1.unit.toBase(w1.value) + w2.unit.toBase(w2.value);
+        return new QuantityWeight(target.fromBase(sum), target);
     }
 }
 
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
-        System.out.println("--- UC8: Refactored Unit Delegation ---");
-        QuantityLength q = new QuantityLength(1, LengthUnit.YARD);
-        System.out.println("1 Yard converted to base through Enum: " + q.getBaseValue());
+        System.out.println("--- UC9: Weight Measurements ---");
+        
+        QuantityWeight kg = new QuantityWeight(1.0, WeightUnit.KG);
+        QuantityWeight grams = new QuantityWeight(1000.0, WeightUnit.GRAM);
+        
+        System.out.println("1 kg == 1000 g: " + kg.equals(grams));
+        
+        QuantityWeight sum = QuantityWeight.add(kg, new QuantityWeight(1.0, WeightUnit.POUND), WeightUnit.KG);
+        System.out.println("1 kg + 1 lb = " + sum.equals(new QuantityWeight(1.453592, WeightUnit.KG)));
     }
 }
