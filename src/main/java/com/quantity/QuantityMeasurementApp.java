94b9c6955c04650efb536a66b89355443f574711
Rakshita Chauhan <chauhanrakshita64@gmail.com>
2026-04-29 03:28:47 +0530

    Add unit conversion method to QuantityLength class

--- src/main/java/com/quantity/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/QuantityMeasurementApp.java
@@ -2,38 +2,25 @@ package com.quantity;
 
 enum LengthUnit {
     FEET(12.0), INCHES(1.0), YARD(36.0), CM(0.393701);
-
     public final double conversionFactor;
     LengthUnit(double factor) { this.conversionFactor = factor; }
 }
 
 class QuantityLength {
-    private final double value;
-    private final LengthUnit unit;
-
-    public QuantityLength(double value, LengthUnit unit) {
-        this.value = value;
-        this.unit = unit;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof QuantityLength)) return false;
-        QuantityLength that = (QuantityLength) obj;
-        return Math.abs((this.value * this.unit.conversionFactor) - 
-                        (that.value * that.unit.conversionFactor)) < 0.001;
+    public static double convert(double value, LengthUnit source, LengthUnit target) {
+        if (!Double.isFinite(value)) return 0.0;
+        return (value * source.conversionFactor) / target.conversionFactor;
     }
 }
 
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
-        System.out.println("--- UC4: Extended Units (Yards & CM) ---");
+        System.out.println("--- UC5: Unit-to-Unit Conversion ---");
         
-        QuantityLength oneYard = new QuantityLength(1.0, LengthUnit.YARD);
-        QuantityLength threeFeet = new QuantityLength(3.0, LengthUnit.FEET);
-        QuantityLength oneCm = new QuantityLength(1.0, LengthUnit.CM);
+        double result = QuantityLength.convert(1.0, LengthUnit.YARD, LengthUnit.INCHES);
+        System.out.println("1 Yard to Inches: " + result);
         
-        System.out.println("1 yard == 3 feet: " + oneYard.equals(threeFeet));
-        System.out.println("1 cm matches inches factor: " + oneCm.equals(new QuantityLength(0.393701, LengthUnit.INCHES)));
+        double cmToFeet = QuantityLength.convert(100.0, LengthUnit.CM, LengthUnit.FEET);
+        System.out.println("100 cm to Feet: " + cmToFeet);
     }
 }
